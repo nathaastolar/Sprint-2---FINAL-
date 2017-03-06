6229b1756945ffa52aa4f86b4530d2c4a8e88946
Natha <natha.astolar@gmail.com>
2017-03-06 15:27:38 -0500

    primer commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-function nombre() {
-  var pedirNombre = document.getElementById("nombre");
-  prompt("¿Cuál es tu nombre?")
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
 }
