20f9a65716442d9765e8147594c1e55a61cab2dc
nathaastolar <natha.astolar@gmail.com>
2017-03-09 17:36:30 -0500

    merge

---
+++ js/quiz1.js
@@ -0,0 +1,26 @@
+
+<h2 "TITULO2" align="left">Sprint 1: Introducción a JavaScript</h2><br>
+        <div id ="TEXTO2"align="left">
+        1. Fundamentos de Programación<br>
+        2. Funciones<br>
+        3. Arreglos<br>
+        4. Objetos<br></div>
+        <h2 align="left">Quiz</h2><br>
+        <h3 align="left">Existen dos tipos de funciones</h3>
+        <div align="left">
+          <input type="radio" name= "GIT" value="1"checked>Expresadas y declaradas<br>
+          <input type="radio" name= "GIT" value="2">Echeckedxplicitas y declaradas <br>
+          <input type="radio" name= "GIT" value="3">Expresadas y derivadas<br>
+        </div>
+        <h3 align="left">Elige una propiedad de los arreglos</h3>
+        <div align="left">
+          <input type="radio" name= "GIT" value="1"checked>clear<br>
+          <input type="radio" name= "GIT" value="2">pop<br>
+          <input type="radio" name= "GIT" value="3">add<br>
+        </div>
+        <h3 align="left">Un objeto tiene ______ y _______</h3>
+        <div align="left">
+          <input type="radio" name= "GIT" value="1"checked>numeros y letras<br>
+          <input type="radio" name= "GIT" value="2">partes y validaciones<br>
+          <input type="radio" name= "GIT" value="3">propiedades y valores<br>
+        </div>
---
+++ js/quiz2.js
@@ -0,0 +1,28 @@
+
+<h2 "TITULO2" align="left">Sprint 2: Introducción al desarrollo web y construcción de páginas web</h2><br>
+      <div id ="TEXTO2"align="left">
+      1. Ciclo de vida del desarrollo web y arquitectura básica cliente/servidor/Configuración del ambiente de trabajo e introducción a git.<br>
+      2. HTML y Javascript en el navegador.<br>
+      3. Manejo de comandos básicos de git en la terminal.<br>
+      4. Pruebas unitarias.<br>
+      5. Git / Resolución de conflictos / Branching model.<br>
+      6. DOM / BOM.<br></div>
+<h2 align="left">Quiz</h2><br>
+<h3 align="left">¿Cuál es el comando para iniciar un repositorio desde la consola?</h3>
+   <div align="left">
+    <input type="radio" name= "GIT" value="1">git init.<br>
+    <input type="radio" name= "GIT" value="2"checked>git init <br>
+    <input type="radio" name= "GIT" value="3">git init add<br>
+   </div>
+    <h3 align="left">¿Cuál es la finalidad de hacer una prueba unitaria?</h3>
+    <div align="left">
+    <input type="radio" name= "GIT" value="1"checked>Comprobar que el código funciona correctamente.<br>
+    <input type="radio" name= "GIT" value="2">Darle valores a los parámetros de dichas funciones.<br>
+    <input type="radio" name= "GIT" value="3">No tiene ningún fin.<br>
+   </div>
+    <h3 align="left">¿Qué significa DOM?</h3>
+    <div align="left">
+    <input type="radio" name= "GIT" value="1">Dual Object Model.<br>
+    <input type="radio" name= "GIT" value="2" checked>Document Objet Model<br>
+    <input type="radio" name= "GIT" value="3">Document Obsolet Model<br>
+  </div>
